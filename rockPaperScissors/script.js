407d45743f398b94f613514cfdeb8e0bd0c6540d
Clark Tyra <clark.tyra@gmail.com>
2021-03-19 13:18:20 -0700

    added makingCalculations function... changed the findWinner function to utilize returns in order to set the winner variable

--- rockPaperScissors/script.js
+++ rockPaperScissors/script.js
@@ -8,10 +8,11 @@ function game() {
   function letsPlay(wantToPlay) {
     if (wantToPlay) {
       getUserChoice();
-      console.log("userChoice: ", userChoice);
       getComputerChoice();
-      console.log("computerChoice: ", computerChoice);
-      findWinner(userChoice, computerChoice);
+      var winner = findWinner(userChoice, computerChoice);
+      makeCalculations(winner)
+      printStats();
+      playAgain()
     } else {
       return;
     }
@@ -34,39 +35,37 @@ function game() {
 
     function findWinner(choice1, choice2) {
       if (choice1 === choice2) {
-        console.log("tie");
-        numOfGames++;
         alert("Tie");
-        printStats();
-        playAgain()
+        return "tie"
       } else if (
         (choice1 === "R" && choice2 === "S") ||
         (choice1 === "P" && choice2 === "R") ||
         (choice1 === "S" && choice2 === "P")
       ) {
-        console.log("User Win");
         alert("User Win");
-        userScore++;
-        numOfGames++;
-        printStats();
-        playAgain()
+        return "user"
       } else {
-        console.log("ComputerWin");
         alert("Computer Win");
-        computerScore++;
-        numOfGames++;
-        printStats();
-        playAgain()
+        return "computer"
       }
+    }
+
+    function makeCalculations(winner){
+        numOfGames++;
+        if(winner === "user"){
+            userScore++;
+        } else if (winner === "computer"){
+            computerScore++;
+        } 
     }
 
     function printStats() {
       alert(
         "numOfGames: " +
           numOfGames +
-          " userScore: " +
+          "\n" + "userScore: " +
           userScore +
-          " computerScore: " +
+          "\n" +"computerScore: " +
           computerScore
       );
     }
@@ -80,20 +79,3 @@ function game() {
   letsPlay(wantToPlay)
 }
 game();
-
-//confirm if you like to play
-//  if true play game
-// else end game
-
-// pick a user choice
-// if choice isn't a possible, ask for user choice again
-
-// generate computer choice
-
-//compare userChoice to computer choice
-//add userScore if user wins
-// add computerScore is computer wins
-//add games played regarless who wins
-
-// ask user if they would like to play again
-//if yes, run playgame, else stop game
