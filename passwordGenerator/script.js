158351ade5c487f7a9db0e7bbae353ca3cf0e977
Clark Tyra <clark.tyra@gmail.com>
2021-03-18 23:31:34 -0700

    added global variables to password js

--- passwordGenerator/script.js
+++ passwordGenerator/script.js
@@ -7,8 +7,39 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  // variables: specialCharacters, numbers, lowerCaseLetters, upperCaseLetters,
+  // isSpecialCharacters, isNumbers, isLoweCaseLetters, isUpperCaseLetters
+  var specialCharacters = [ "!",  `"`, "#", "$", "%", "&", `'`, "(",  ")",  "*", "+", ",",  "-", ".", "/", ":", ";", "<", "=", ">", "?",  "@", "[", `\`, "]", `^`, "_", ```, `{`, `|`, `}`, `~` ];
+  var numbers = [1,2,3,4,5,6,7,8,9,0];
+  var lowerCaseLetters = "abcdefghijklmnopgrstuvwzyz";
+  var upperCaseLetters = lowerCaseLetters.toUpperCase();
+  var isSpecialCharacters, isNumbers, isLoweCaseLetters, isUpperCaseLetters = false;
+
+}
+
+// GIVEN I need a new, secure password click the button to generate a password
+// presented with a series of prompts for password criteria
+
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
