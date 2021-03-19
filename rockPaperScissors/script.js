17838a267e383b31322147a7e202fd729c5a94a9
Clark Tyra <clark.tyra@gmail.com>
2021-03-19 10:38:03 -0700

    added getUserChoice function

--- rockPaperScissors/script.js
+++ rockPaperScissors/script.js
@@ -1,5 +1,25 @@
 function game(){
-    var wantToPlay = confirm("Would you like to play Rock, Paper, Scissors?")
+    var userChoice;
+    var userScore;
+    var computerChoice;
+    var computerScore;
+    var numOfGames;
+    var wantToPlay = confirm("Would you like to play Rock, Paper, Scissors?");
+    if(wantToPlay) {
+        getUserChoice();
+    } else{
+        return
+    }
+
+    function getUserChoice(){
+        userChoice = prompt("what is your choice? (R, P, or S)")
+        console.log(userChoice)
+        console.log(typeof(userChoice))
+        if (userChoice !== "R" && userChoice !== "P" && userChoice !== "S" ){
+            alert("Sorry, choice has to be R, P, or S")
+            getUserChoice()
+        }
+    }
 }
 game()
 
