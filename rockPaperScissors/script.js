97ddd3d3bd17e158f3dd4e6e2250aa0e39b43576
Clark Tyra <clark.tyra@gmail.com>
2021-03-19 10:27:01 -0700

    added psuedo code for Rock Paper Scissors

---
+++ rockPaperScissors/script.js
@@ -0,0 +1,21 @@
+function game(){
+    var wantToPlay = confirm("Would you like to play Rock, Paper, Scissors?")
+}
+game()
+
+//confirm if you like to play
+//  if true play game
+// else end game
+
+// pick a user choice
+    // if choice isn't a possible, ask for user choice again
+
+// generate computer choice
+
+//compare userChoice to computer choice
+//add userScore if user wins
+// add computerScore is computer wins
+//add games played regarless who wins
+
+// ask user if they would like to play again
+//if yes, run playgame, else stop game
