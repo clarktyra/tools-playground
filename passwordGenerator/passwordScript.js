18404ebb1326c52e0e72e61cd0c9cc0b5d3e31a5
Clark Tyra <clark.tyra@gmail.com>
2021-03-19 01:11:51 -0700

    refactored createList function for less lines of code

--- passwordGenerator/passwordScript.js
+++ passwordGenerator/passwordScript.js
@@ -1,14 +1,14 @@
 const generateBtn = document.querySelector("#generate");
 
- writePassword = () => {
+writePassword = () => {
   const password = generatePassword();
   const passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-}
+};
+
 generateBtn.addEventListener("click", writePassword);
 
- generatePassword = () => {
+generatePassword = () => {
   const specialCharacters = [
     "@",
     "%",
@@ -34,64 +34,67 @@ generateBtn.addEventListener("click", writePassword);
     "_",
     ".",
   ];
-  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const lowerCaseLetters = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z'
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
   ];
   const upperCaseLetters = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z'
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
   ];
-  let isSpecialCharacters, isNumbers, isLoweCaseLetters, isUpperCaseLetters = false;
+  let isSpecialCharacters,
+    isNumbers,
+    isLoweCaseLetters,
+    isUpperCaseLetters = false;
   let password = "";
   let passwordLength = 0;
 
@@ -104,47 +107,51 @@ generateBtn.addEventListener("click", writePassword);
     if (passwordLength >= 8 && passwordLength <= 128) {
       return;
     } else {
-      alert("Please give a length between 8-128.")
+      alert("Please give a length between 8-128.");
       getPasswordLength();
     }
-  }
+  };
   getPasswordLength();
 
   getPasswordCriteria = () => {
     isSpecialCharacters = confirm("Do you want special charcters?");
-    isNumbers           = confirm("Do you want numbers?");
-    isLoweCaseLetters   = confirm("Do you want lower case letters?");
-    isUpperCaseLetters  = confirm("Do you want upper case letters?");
-    if (!isSpecialCharacters && !isNumbers && !isLoweCaseLetters && !isUpperCaseLetters){
-      alert("At least 1 type(specials, numbers, characters) needs to be allowed.")
+    isNumbers = confirm("Do you want numbers?");
+    isLoweCaseLetters = confirm("Do you want lower case letters?");
+    isUpperCaseLetters = confirm("Do you want upper case letters?");
+    if (
+      !isSpecialCharacters &&
+      !isNumbers &&
+      !isLoweCaseLetters &&
+      !isUpperCaseLetters
+    ) {
+      alert(
+        "At least 1 type(specials, numbers, characters) needs to be allowed."
+      );
       getPasswordCriteria();
     }
-  }
+  };
   getPasswordCriteria();
 
   createList = (array1, array2, array3, array4) => {
     var array = [];
-    if(array1){
-      array = array.concat(specialCharacters)
-    }
-    if(array2){
-      array = array.concat(numbers)
-    }
-    if(array3){
-      array = array.concat(lowerCaseLetters)
-    }
-    if(array4){
-      array = array.concat(upperCaseLetters)
-    }
+    if (array1) array = array.concat(specialCharacters);
+    if (array2) array = array.concat(numbers);
+    if (array3) array = array.concat(lowerCaseLetters);
+    if (array4) array = array.concat(upperCaseLetters);
     return array;
-  }
-  const listOfCharacters = createList(isSpecialCharacters, isNumbers, isLoweCaseLetters, isUpperCaseLetters);
+  };
+  const listOfCharacters = createList(
+    isSpecialCharacters,
+    isNumbers,
+    isLoweCaseLetters,
+    isUpperCaseLetters
+  );
 
-  for (let i = 0; i < passwordLength; i++){
+  for (let i = 0; i < passwordLength; i++) {
     const lenghtOfList = listOfCharacters.length;
-    const getRandomIndex = Math.floor(Math.random()* lenghtOfList)
-    password += listOfCharacters[getRandomIndex]
+    const getRandomIndex = Math.floor(Math.random() * lenghtOfList);
+    password += listOfCharacters[getRandomIndex];
   }
 
-  return password
-}
+  return password;
+};
