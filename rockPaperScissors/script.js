317af731538f1dd6cdda1ee322b649839546482b
Clark Tyra <clark.tyra@gmail.com>
2021-03-19 20:31:02 -0700

    changed functions to arrow functions, changed vars to const and let, moves game logic to the bottom because of a scoping issue

--- rockPaperScissors/script.js
+++ rockPaperScissors/script.js
@@ -1,81 +1,77 @@
-function game() {
-  var userChoice;
-  var userScore = 0;
-  var computerChoice;
-  var computerScore = 0;
-  var numOfGames = 0;
-  var wantToPlay = confirm("Would you like to play Rock, Paper, Scissors?");
-  function letsPlay(wantToPlay) {
-    if (wantToPlay) {
-      getUserChoice();
-      getComputerChoice();
-      var winner = findWinner(userChoice, computerChoice);
-      makeCalculations(winner)
-      printStats();
-      playAgain()
-    } else {
-      return;
-    }
-
-    function getUserChoice() {
-      userChoice = prompt("what is your choice? (R, P, or S)");
-      console.log(userChoice);
-      console.log(typeof userChoice);
-      if (userChoice !== "R" && userChoice !== "P" && userChoice !== "S") {
+game = () => {
+  let userScore = 0;
+  let computerScore = 0;
+  let numOfGames = 0;
+  let wantToPlay = confirm("Would you like to play Rock, Paper, Scissors?");
+  
+  letsPlay = (wantToPlay) => {
+    getUserChoice = () => {
+      const choice = prompt("what is your choice? (R, P, or S)");
+      if (choice !== "R" && choice !== "P" && choice !== "S") {
         alert("Sorry, choice has to be R, P, or S");
         getUserChoice();
       }
-    }
-
-    function getComputerChoice() {
-      var choiceArray = ["R", "P", "S"];
-      var index = Math.floor(Math.random() * 3);
-      computerChoice = choiceArray[index];
-    }
-
-    function findWinner(choice1, choice2) {
+      return choice;
+    };
+    getComputerChoice = () => {
+      const choiceArray = ["R", "P", "S"];
+      const index = Math.floor(Math.random() * 3);
+      const choice = choiceArray[index];
+      return choice;
+    };
+    findWinner = (choice1, choice2) => {
       if (choice1 === choice2) {
         alert("Tie");
-        return "tie"
+        return "tie";
       } else if (
         (choice1 === "R" && choice2 === "S") ||
         (choice1 === "P" && choice2 === "R") ||
         (choice1 === "S" && choice2 === "P")
       ) {
         alert("User Win");
-        return "user"
+        return "user";
       } else {
         alert("Computer Win");
-        return "computer"
+        return "computer";
       }
-    }
-
-    function makeCalculations(winner){
-        numOfGames++;
-        if(winner === "user"){
-            userScore++;
-        } else if (winner === "computer"){
-            computerScore++;
-        } 
-    }
-
-    function printStats() {
+    };
+    makeCalculations = (winner) => {
+      numOfGames++;
+      if (winner === "user") {
+        userScore++;
+      } else if (winner === "computer") {
+        computerScore++;
+      }
+    };
+    printStats = () => {
       alert(
         "numOfGames: " +
           numOfGames +
-          "\n" + "userScore: " +
+          "\n" +
+          "userScore: " +
           userScore +
-          "\n" +"computerScore: " +
+          "\n" +
+          "computerScore: " +
           computerScore
       );
-    }
+    };
+    playAgain = () => {
+      const wantToPlayAgain = confirm("Would you like to play again");
+      letsPlay(wantToPlayAgain);
+    };
 
-    function playAgain(){
-        var wantToPlayAgain = confirm("Would you like to play again")
-        letsPlay(wantToPlayAgain)
+    if (wantToPlay) {
+      const userChoice = getUserChoice();
+      const computerChoice = getComputerChoice();
+      const winner = findWinner(userChoice, computerChoice);
+      makeCalculations(winner);
+      printStats();
+      playAgain();
+    } else {
+      return;
     }
-  }
+  };
 
-  letsPlay(wantToPlay)
-}
+  letsPlay(wantToPlay);
+};
 game();
