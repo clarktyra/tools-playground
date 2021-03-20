6470d7c2742053783dffcde95d2ea392564982e4
Clark Tyra <clark.tyra@gmail.com>
2021-03-19 21:10:37 -0700

    added timer functionality

--- wordGuessGame/logic.js
+++ wordGuessGame/logic.js
@@ -1,21 +1,38 @@
 //click button
-    //starts timer
-    //starts game
+//starts timer
+//starts game
 
 //game
-    //get a random word
-    //display blanks of word
-    //press keys
-        //if keypress exists in word, then letter shows up in that spot
-    //if the whole word is displayed
-        //timer stops
-        //save user win
-    //if timer hits 0
-        //save user lose
-        //timer stops
+//get a random word
+//display blanks of word
+//press keys
+//if keypress exists in word, then letter shows up in that spot
+//if the whole word is displayed
+//timer stops
+//save user win
+//if timer hits 0
+//save user lose
+//timer stops
 
 //game over
-    //dipsplay wins and lossws
+//dipsplay wins and lossws
 
 //refresh page
-    //still display wins and losses
+//still display wins and losses
+
+var time;
+
+var buttonEl = document.querySelector("button");
+var timerEl = document.querySelector(".timer");
+buttonEl.addEventListener("click", countdownTimer);
+
+function countdownTimer() {
+  time = 6;
+  var countdown = setInterval(function () {
+    time--;
+    timerEl.textContent = time;
+    if(time <= 0) {
+        clearInterval(countdown)
+    }
+  }, 1000);
+}
