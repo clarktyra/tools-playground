04e7b290a1cbccec5ed21fa6de95bc4fbbe51998
Clark Tyra <clark.tyra@gmail.com>
2021-03-19 20:48:07 -0700

    added logic js to hangman folder, added psuedo code

---
+++ wordGuessGame/logic.js
@@ -0,0 +1,21 @@
+//click button
+    //starts timer
+    //starts game
+
+//game
+    //get a random word
+    //display blanks of word
+    //press keys
+        //if keypress exists in word, then letter shows up in that spot
+    //if the whole word is displayed
+        //timer stops
+        //save user win
+    //if timer hits 0
+        //save user lose
+        //timer stops
+
+//game over
+    //dipsplay wins and lossws
+
+//refresh page
+    //still display wins and losses
