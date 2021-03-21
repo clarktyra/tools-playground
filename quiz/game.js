e4212109dca1b2c3e134874a6494e84fb3e1c7e6
Clark Tyra <clark.tyra@gmail.com>
2021-03-21 12:59:20 -0700

    added pseudo code for code Quiz

---
+++ quiz/game.js
@@ -0,0 +1,30 @@
+// game logic psuedo code
+
+// GAME PAGE
+// press start quiz button
+    //countdown timer starts
+    //display countdown timer
+    //display first question, with 4 possible answers as buttons
+        //click on an answer button
+            //if answer is incorrect, subtract time from timer
+            //new question appears
+    // if all questions answered or timer reaches 0 
+        // game over
+            //save initials and score
+            //go to highscores page
+
+// HIGHSCORES PAGE
+// diplay high scores and initials in a list
+
+// VARIABLES
+// timer, questions array of objects, initials, score
+
+// FUNCTIONS
+// start, displayQuestion, checkAnswer, quizOverDisplay
+           
+           
+           
+           
+           
+           
+          
