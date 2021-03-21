9b4b2ed3d18b3e4f9ee33612b997c004ac9ada36
Clark Tyra <clark.tyra@gmail.com>
2021-03-21 13:13:42 -0700

    created my document elements, my variables timer, questions, initials, and score, also creat start function and tested the startbutton onclick

--- quiz/game.js
+++ quiz/game.js
@@ -2,16 +2,17 @@
 
 // GAME PAGE
 // press start quiz button
-    //countdown timer starts
-    //display countdown timer
-    //display first question, with 4 possible answers as buttons
-        //click on an answer button
-            //if answer is incorrect, subtract time from timer
-            //new question appears
-    // if all questions answered or timer reaches 0 
-        // game over
-            //save initials and score
-            //go to highscores page
+//countdown timer starts
+//display countdown timer
+//hide the start screen
+//display first question, with 4 possible answers as buttons
+//click on an answer button
+//if answer is incorrect, subtract time from timer
+//new question appears
+// if all questions answered or timer reaches 0
+// game over
+//save initials and score
+//go to highscores page
 
 // HIGHSCORES PAGE
 // diplay high scores and initials in a list
@@ -21,10 +22,59 @@
 
 // FUNCTIONS
 // start, displayQuestion, checkAnswer, quizOverDisplay
-           
-           
-           
-           
-           
-           
-          
+
+const timerEl = document.getElementById("time");
+const startBtn = document.getElementById("start");
+const startScreen = document.getElementById("start-screen");
+const questionScreen = document.getElementById("questions");
+const questionTitleEl = document.getElementById("questions-title");
+const choicesEl = document.getElementById("choices");
+const endScreen = document.getElementById("end-screen");
+const finalScrArea = document.getElementById("final-score");
+const initialsBtn = document.getElementById("submit");
+
+let timer = 30;
+const questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    title:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes",
+  },
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log",
+  },
+];
+let initials;
+let score;
+
+start = () => {
+    alert("start")
+}
+
+startBtn.onclick = start;
+
+
