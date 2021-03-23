39675463d31dea10ade42c0f8addb24019e735aa
Clark Tyra <clark.tyra@gmail.com>
2021-03-23 13:01:57 -0700

    created simple ReadME, added Psuedocode text file, made pseufo code

---
+++ weatherDashboard/script.js
@@ -0,0 +1,23 @@
+// search for a city
+//     city is added to the search history
+//         click on a city in the search history
+//             I am again presented with current and future conditions for that city
+//     presented with current   
+//         city name 
+//         the date
+//         an icon representation of weather conditions 
+//         the temperature 
+//         the humidity
+//         the wind speed
+//         the UV index
+//             presented with a color that indicates whether the conditions are favorable, moderate, or severe
+//     future 5-day forecast conditions for that city
+//         the date
+//         an icon representation of weather conditions
+//         the temperature
+//         and the humidity
+
+//Pseudo Code
+
+
+var apiKey = "003a54ddfe39c875d00206109efba335"
