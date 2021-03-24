e7d2b5064d8f7656b0d5729c1d7d3bc0a60ce0e3
Clark Tyra <clark.tyra@gmail.com>
2021-03-23 20:58:11 -0700

    single day forcast all working

--- weatherDashboard/script.js
+++ weatherDashboard/script.js
@@ -21,74 +21,108 @@ window.addEventListener("load", function () {
     {
       date: "March 2, 2020",
       icon: "icon*",
-      tmperature: "100 degrees",
+      temperature: "100 degrees",
       humidity: "50%",
     },
     {
       date: "March 3, 2020",
       icon: "icon*",
-      tmperature: "100 degrees",
+      temperature: "100 degrees",
       humidity: "50%",
     },
     {
       date: "March 4, 2020",
       icon: "icon*",
-      tmperature: "100 degrees",
+      temperature: "100 degrees",
       humidity: "50%",
     },
     {
       date: "March 5, 2020",
       icon: "icon*",
-      tmperature: "100 degrees",
+      temperature: "100 degrees",
       humidity: "50%",
     },
     {
       date: "March 6, 2020",
       icon: "icon*",
-      tmperature: "100 degrees",
+      temperature: "100 degrees",
       humidity: "50%",
     },
   ];
+  var cities = [];
 
   searchButtonEl.addEventListener("click", function () {
+    console.log("searchValueEl.value : ", searchValueEl.value);
+    cityName = searchValueEl.value;
     storeCityHistory(cityName);
-    diplayTodayWeather(citOneDayforecast);
+    diplayTodayWeather(cityName);
     diplay5DayForcast(fiveDayForecast);
   });
 
   function storeCityHistory(cN) {
-    console.log("cN: ", cN)
+    console.log("cN: ", cN);
+    cities.push(cN);
+    localStorage.setItem("cities", JSON.stringify(cities));
     diplayCityHistory();
   }
 
   function diplayCityHistory() {
-
+    historyEl.innerHTML = "";
+    var myCityList = JSON.parse(localStorage.getItem("cities"));
+    myCityList.reverse();
+    for (let piece of myCityList) {
+      var oneCity = document.createElement("p");
+      oneCity.innerHTML = piece;
+      historyEl.append(oneCity);
+    }
   }
 
-  function diplayTodayWeather(c1DF) {
-    console.log("cN: ", c1DF)
-    var oneDayCard = document.createElement("div") 
-    for(let piece in c1DF){
-        var oneDayP = document.createElement("p") 
-        oneDayP.innerHTML = c1DF[piece]
-        oneDayCard.append(oneDayP)
-    }
-    todayEl.append(oneDayCard)
-
+  function diplayTodayWeather(cN) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cN}&appid=${apiKey}`
+    )
+      .then((res) => res.json())
+      .then((data) => data)
+      .then((data) => {
+        var dataObj = {
+          cityName: data.name,
+          date: moment().format("LL"),
+          icon: data.weather[0].icon,
+          temperature: data.main.temp,
+          humidity: data.main.humidity,
+          windSpeed: data.wind.speed,
+          uvIndex: "99uv",
+        };
+        fetch(
+          `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${data.coord.lat}&lon=${data.coord.lon}`
+        )
+          .then((res) => res.json())
+          .then((data1) => {
+            console.log("data1: ", data1);
+            dataObj.uvIndex = data1.value;
+            var c1DF = dataObj;
+            console.log("cN: ", c1DF);
+            var oneDayCard = document.createElement("div");
+            for (let piece in c1DF) {
+              var oneDayP = document.createElement("p");
+              oneDayP.innerHTML = c1DF[piece];
+              oneDayCard.append(oneDayP);
+            }
+            todayEl.append(oneDayCard);
+          });
+      });
   }
 
   function diplay5DayForcast(fiveDF) {
-    console.log("fiveDF: ", fiveDF)
-    for(let piece of fiveDF ){
-        var oneFifthCard = document.createElement("div") 
-        for(let key in piece ){
-            var oneDayP = document.createElement("p") 
-            oneDayP.innerHTML = piece[key]
-            oneFifthCard.append(oneDayP)
-        }
-        forecastEl.append(oneFifthCard)
+    console.log("fiveDF: ", fiveDF);
+    for (let piece of fiveDF) {
+      var oneFifthCard = document.createElement("div");
+      for (let key in piece) {
+        var oneDayP = document.createElement("p");
+        oneDayP.innerHTML = piece[key];
+        oneFifthCard.append(oneDayP);
+      }
+      forecastEl.append(oneFifthCard);
     }
-
-
   }
 });
