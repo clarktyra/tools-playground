4c88525d4bfa982256ed6cba09ebfd40d611aef3
Clark Tyra <clark.tyra@gmail.com>
2021-03-24 09:15:24 -0700

    cleaned up code, took out unnessary things, changed vars to lets and consts

--- weatherDashboard/script.js
+++ weatherDashboard/script.js
@@ -1,71 +1,20 @@
 window.addEventListener("load", function () {
-  var apiKey = "003a54ddfe39c875d00206109efba335";
-  //set up the dom elements
+  const apiKey = "003a54ddfe39c875d00206109efba335";
   const searchButtonEl = document.getElementById("search-button");
   const searchValueEl = document.querySelector("#search-value");
   const historyEl = document.querySelector("#history");
   const todayEl = document.querySelector("#today");
   const forecastEl = document.querySelector("#forecast");
-//   const cityClickedEl = document.getElementsByClassName("cityClicked");
-
-  
-
-  var cityName = "Dallas";
-  var citOneDayforecast = {
-    cityName: "Dallas",
-    date: "March 2, 2020",
-    icon: "icon*",
-    tmperature: "100 degrees",
-    humidity: "50%",
-    windSpeed: "15 mph",
-    uvIndex: "99uvi",
-  };
-  var fiveDayForecast = [
-    {
-      date: "March 2, 2020",
-      icon: "icon*",
-      temperature: "100 degrees",
-      humidity: "50%",
-    },
-    {
-      date: "March 3, 2020",
-      icon: "icon*",
-      temperature: "100 degrees",
-      humidity: "50%",
-    },
-    {
-      date: "March 4, 2020",
-      icon: "icon*",
-      temperature: "100 degrees",
-      humidity: "50%",
-    },
-    {
-      date: "March 5, 2020",
-      icon: "icon*",
-      temperature: "100 degrees",
-      humidity: "50%",
-    },
-    {
-      date: "March 6, 2020",
-      icon: "icon*",
-      temperature: "100 degrees",
-      humidity: "50%",
-    },
-  ];
-  var cities = [];
+  let cities = [];
 
   searchButtonEl.addEventListener("click", function () {
-    console.log("searchValueEl.value : ", searchValueEl.value);
-    cityName = searchValueEl.value;
+    const cityName = searchValueEl.value;
     storeCityHistory(cityName);
     diplayTodayWeather(cityName);
     diplay5DayForcast(cityName);
-  });
-
-  
+  })
 
   function storeCityHistory(cN) {
-    console.log("cN: ", cN);
     cities.push(cN);
     localStorage.setItem("cities", JSON.stringify(cities));
     diplayCityHistory();
@@ -73,14 +22,13 @@ window.addEventListener("load", function () {
 
   function diplayCityHistory() {
     historyEl.innerHTML = "";
-    var myCityList = JSON.parse(localStorage.getItem("cities"));
+    let myCityList = JSON.parse(localStorage.getItem("cities"));
     myCityList.reverse();
     for (let piece of myCityList) {
-      var oneCity = document.createElement("p");
+      const oneCity = document.createElement("p");
       oneCity.innerHTML = piece;
       oneCity.addEventListener("click", function(event){
         event.preventDefault()
-        console.log(event.target.textContent)
         diplayTodayWeather(event.target.textContent);
         diplay5DayForcast(event.target.textContent)
       })
@@ -94,9 +42,8 @@ window.addEventListener("load", function () {
       `https://api.openweathermap.org/data/2.5/weather?q=${cN}&appid=${apiKey}`
     )
       .then((res) => res.json())
-      .then((data) => data)
       .then((data) => {
-        var dataObj = {
+        const dataObj = {
           cityName: data.name,
           date: moment().format("LL"),
           icon: data.weather[0].icon,
@@ -110,14 +57,12 @@ window.addEventListener("load", function () {
         )
           .then((res) => res.json())
           .then((data1) => {
-            console.log("data1: ", data1);
             dataObj.uvIndex = data1.value;
-            var c1DF = dataObj;
-            console.log("cN: ", c1DF);
-            var oneDayCard = document.createElement("div");
+            const c1DF = dataObj;
+            const oneDayCard = document.createElement("div");
             for (let piece in c1DF) {
                 if (piece === "icon") {
-                    var oneImage = document.createElement("img");
+                    const oneImage = document.createElement("img");
                     oneImage.setAttribute(
                       "src",
                       `http://openweathermap.org/img/w/${c1DF.icon}.png`
@@ -137,33 +82,30 @@ window.addEventListener("load", function () {
 
   function diplay5DayForcast(cN) {
     forecastEl.innerHTML = "";
-    var fiveDF = fiveDayForecast;
     fetch(
       `https://api.openweathermap.org/data/2.5/forecast?q=${cN}&appid=${apiKey}`
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log("fiveData : ", data);
         for (let i = 0; i < 40; i += 8) {
-          console.log("data.list[i] : ", data.list[i]);
-          var myObj = {
+          const myObj = {
             date: data.list[i].dt_txt,
             icon: data.list[i].weather[0].icon,
             temperature: data.list[i].main.temp,
             humidity: data.list[i].main.humidity,
           };
 
-          var oneFifthCard = document.createElement("div");
+          const oneFifthCard = document.createElement("div");
           for (let key in myObj) {
             if (key === "icon") {
-              var oneImage = document.createElement("img");
+              const oneImage = document.createElement("img");
               oneImage.setAttribute(
                 "src",
                 `http://openweathermap.org/img/w/${myObj.icon}.png`
               );
               oneFifthCard.append(oneImage);
             } else {
-              var oneDayP = document.createElement("p");
+              const oneDayP = document.createElement("p");
               oneDayP.innerHTML = myObj[key];
               oneFifthCard.append(oneDayP);
             }
