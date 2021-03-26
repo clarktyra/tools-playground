8c63d48e4b3fcbe65bdbccbe1aa9a033bf30cb4d
Clark Tyra <clark.tyra@gmail.com>
2021-03-25 23:53:51 -0700

    added planet images, that show up for each one, took out the comments of the code, reversd the planets array so it starts with the sun first

--- astro-weight/main.js
+++ astro-weight/main.js
@@ -1,73 +1,91 @@
-// Write your JavaScript code here!
 var planets = [
-    ['Pluto', 0.06],
-    ['Neptune', 1.148],
-    ['Uranus', 0.917],
-    ['Saturn', 1.139],
-    ['Jupiter', 2.640],
-    ['Mars', 0.3895],
-    ['Moon', 0.1655],
-    ['Earth', 1],
-    ['Venus', 0.9032],
-    ['Mercury', 0.377],
-    ['Sun', 27.9]
-  ];
-  // We're going to solve this by breaking the problem into three parts.
-// Programmers like automating things, we'll populate the HTML drop down options using code,
-// instead of having to type out all the values.
+  [
+    "Pluto",
+    0.06,
+    "https://images.unsplash.com/photo-1614314107768-6018061b5b72?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDM4ODV8MHwxfHNlYXJjaHwxfHxQbHV0b3xlbnwwfHx8fDE2MTY3MzkxMjA&ixlib=rb-1.2.1&q=85",
+  ],
+  [
+    "Neptune",
+    1.148,
+    "https://images.unsplash.com/photo-1614313913007-2b4ae8ce32d6?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDM4ODV8MHwxfHNlYXJjaHwxfHxOZXB0dW5lfGVufDB8fHx8MTYxNjczOTE0MQ&ixlib=rb-1.2.1&q=85",
+  ],
+  [
+    "Uranus",
+    0.917,
+    "https://images.unsplash.com/photo-1614732484003-ef9881555dc3?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDM4ODV8MHwxfHNlYXJjaHwxfHxVcmFudXN8ZW58MHx8fHwxNjE2NzM5MTgx&ixlib=rb-1.2.1&q=85",
+  ],
+  [
+    "Saturn",
+    1.139,
+    "https://images.unsplash.com/photo-1614732414444-096e5f1122d5?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDM4ODV8MHwxfHNlYXJjaHwxfHxTYXR1cm58ZW58MHx8fHwxNjE2NzM5MTkx&ixlib=rb-1.2.1&q=85",
+  ],
+  [
+    "Jupiter",
+    2.64,
+    "https://www.nasa.gov/sites/default/files/thumbnails/image/stsci-h-p1936a-m-1999x2000.png",
+  ],
+  [
+    "Mars",
+    0.3895,
+    "https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg",
+  ],
+  [
+    "Moon",
+    0.1655,
+    "https://images.unsplash.com/photo-1532693322450-2cb5c511067d?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDM4ODV8MHwxfHNlYXJjaHwxMXx8TW9vbnxlbnwwfHx8fDE2MTY3MzkxNjU&ixlib=rb-1.2.1&q=85",
+  ],
+  [
+    "Earth",
+    1,
+    "https://i1.wp.com/www.travindy.com/wp-content/uploads/2018/04/Planet-Earth-Article-Image.jpg?fit=1920%2C1920&ssl=1",
+  ],
+  [
+    "Venus",
+    0.9032,
+    "https://upload.wikimedia.org/wikipedia/commons/a/a9/PIA23791-Venus-NewlyProcessedView-20200608.jpg",
+  ],
+  [
+    "Mercury",
+    0.377,
+    "https://upload.wikimedia.org/wikipedia/commons/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg",
+  ],
+  [
+    "Sun",
+    27.9,
+    "https://i.insider.com/5d51622e558a356f0b728a13?width=1053&format=jpeg",
+  ],
+];
 
-//Create a function that does the some math and gives us the new weight.
-
-//Then create a function that responds when the user clicks on the button.
-
-// //1. Populate the dropdown element with the data found in the planets array.
-// The value of each option should be the planet's name.
-// Use the following built-in methods:
-// `.forEach` `document.createElement` `document.getElementById` `.appendChild`
-
-
-
-planets.forEach(function(planet) {
-    var element1 = document.createElement("option");
-    element1.text = planet[0];
-    element1.value = planet[0];
-    document.getElementById("planets").appendChild(element1);
+planets.reverse()
+planets.forEach(function (planet) {
+  var element1 = document.createElement("option");
+  element1.text = planet[0];
+  element1.value = planet[0];
+  document.getElementById("planets").append(element1);
 });
 
-
 function calculateWeight(weight, planetName) {
-    // 2. Write the code to return the correct weight
-    for (var i = 0; i < planets.length; i++) { 
-        if (planets[i][0] == planetName) {
-            var multiplier = planets[i][1];
-            
-        }
+  for (var i = 0; i < planets.length; i++) {
+    if (planets[i][0] == planetName) {
+      var multiplier = planets[i][1];
     }
-    return multiplier * weight;
   }
-  
-  function handleClickEvent(e) {
-    e.preventDefault()
-    // 3. Create a variable called userWeight and assign the value of the user's weight.
-  var userWeight = $("#user-weight").val();
-    // 4. Create a variable called planetName and assign the name of the selected planet from the drop down.
-  var planetName = $("#planets").val();
-    // 5. Create a variable called result and assign the value of the new calculated weight.
-  var result = calculateWeight(userWeight, planetName);
-    // 6. Write code to display the message shown in the screenshot.
-    //document.getElementById("output").innerHTML= "If you were" + planetName + "you would weigh" + result + "lbs";
-    $("#output").text(" If you were on " + planetName +", you would weigh "+ result + "lbs!");
+  return multiplier * weight;
 }
-  
-//   7. Set the #calculate-button element's onclick method to use the handleClickEvent function.
-$("#calculate-button").click(handleClickEvent);
-//   Bonus Challenges
-//   8. Reverse the drop down order so that the sun is first.
-//   9. Make it look nice using bootstrap (http://getbootstrap.com/getting-started/)
 
-// .forEach takes the array planets and breaks it up into a set of individual elements
-// creating an varialble element1 that is new that gives options
-// the  indexes of the array that are text, so it is all the planet names
-// the indexes of the array that are values, so it is all rate of change
-// returns elements from the property planets, adds text and value
-  
+function handleClickEvent(e) {
+  e.preventDefault();
+  var userWeight = $("#user-weight").val();
+  var planetName = $("#planets").val();
+  var result = calculateWeight(userWeight, planetName);
+  $("#output").text(
+    " If you were on " + planetName + ", you would weigh " + result + "lbs!"
+  )
+  for (let piece of planets) {
+    if (planetName === piece[0]) {
+      $("#outputImage").attr("src", piece[2]);
+    }
+  }
+}
+
+$("#calculate-button").click(handleClickEvent);
